e3173c345bd8cae435a6552692e4b50ba178688b
braman22 <bramanbarbara@gmail.com>
2025-12-14 18:53:02 +0700

    AI: Complete function in first-braman

--- js/snippets.js
+++ js/snippets.js
@@ -66,3 +66,8 @@ export function delay(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+
+export function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
